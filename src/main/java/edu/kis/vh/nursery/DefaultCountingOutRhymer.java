40d04bb6e1dc1fe2a0d7a98cb38ec359894aa72a
GorkaMaciej <251143@edu.p.lodz.pl>
2025-03-17 15:19:37 +0100

    6. Zamiana dostepnych literalow na stale

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int ERROR = -1;
 
-    public int total = -1;
+	private static final int NUMBERS_CAPACITY = 11;
+
+	private int[] numbers = new int[12]; 
+    
+    public int total = ERROR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == NUMBERS_CAPACITY;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total--];
     }
 
