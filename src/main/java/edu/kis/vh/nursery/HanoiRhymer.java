4d895245238c63e9e82caba4124387704f7c6d12
Witold Struminski <251227@edu.p.lodz.pl>
2025-03-31 14:49:25 +0200

    17. Dodano dokumentacje HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,28 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Rhymer that rejects elements smaller or equal
+ * to the top element, like the Hanoi Tower
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * @return Amount of rejected inputs
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Add param in to the rhymer if it's bigger than the last,
+     * or it's the first element.
+     * In other case reject it and increase the amount of rejected
+     * inputs.
+     *
+     * @param in - input for the Rhymer
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
