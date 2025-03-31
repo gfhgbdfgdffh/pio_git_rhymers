df687598b762dce968e60b3e11acdca634c0d187
Witold Struminski <251227@edu.p.lodz.pl>
2025-03-31 14:35:40 +0200

    14. Usuniecie Node.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,4 +38,33 @@ public class IntLinkedList {
         return ret;
     }
 
+    private static class Node {
+
+        private final int value;
+        private Node prev, next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
